2e9df8440f6ed51eae4e28b7fef74512477e661a
Jpablo55 <135130507+Jpablo55@users.noreply.github.com>
2025-06-17 07:50:32 -0500

    Add files via upload

---
+++ M3_S1/Intro JS/5-condicionales.js
@@ -0,0 +1,27 @@
+// 1
+// let numero = 20;
+// let numeroUsuario = prompt("Ingresa un número");
+// if(numero > numeroUsuario){
+//     console.log(`El número ingresado (${numeroUsuario}) no es mayor a ${numero}`);
+// } 
+// else{
+//     console.log(`El número ingresado (${numeroUsuario}) es mayor a ${numero}`);
+// }
+
+// 2
+// let nombreUsuario = prompt("Ingresa el nombre de usuario");
+// let admin = 'Admin';
+// if(nombreUsuario === admin){
+//     console.log("Bienvenido, administrador! Tu acceso ha sido concedido. ¡Que comience el control total! ");
+// }
+// else{
+//     console.log("Mensaje genérico");
+// }
+
+// 3
+// let numeroIngresado = parseFloat(prompt("Ingresa un número"));
+// let modulo = numeroIngresado % 2
+// let isPar = modulo === 0
+// console.log(`El número ingresado es ${isPar ? "par": "impar"}`)
+
+
---
+++ M3_S1/Intro JS/8-desafio_final.js
@@ -0,0 +1,4 @@
+let nombre = prompt("Ingresa tu nombre")
+let edad = prompt("Ingresa tu edad")
+let ciudad = prompt("Ingresa la ciudad donde vives")
+console.log(`Hola ${nombre}, tienes ${edad} años y vives en ${ciudad}.`)

0f83ffe03148626698e5b8434f2f27d22f06ffea
Jpablo55 <135130507+Jpablo55@users.noreply.github.com>
2025-06-17 07:47:28 -0500

    Delete M3_S1/Intro JS directory

--- M3_S1/Intro JS/desafio_final.js
+++
@@ -1,4 +0,0 @@
-let nombre = prompt("Ingresa tu nombre")
-let edad = prompt("Ingresa tu edad")
-let ciudad = prompt("Ingresa la ciudad donde vives")
-console.log(`Hola ${nombre}, tienes ${edad} años y vives en ${ciudad}.`)
--- M3_S1/Intro JS/entrada_y_salida_datos.js
+++
@@ -1,9 +0,0 @@
-// Prompt
-let edadIngresada = prompt("Ingresa tu edad")
-console.log(`Tienes ${edadIngresada}`)
-
-// Alert 
-alert("Alerta")
-
-// Confirm 
-confirm("Deseas continuar?")

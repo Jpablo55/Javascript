32d2a75b45d3ecbeb7bdbec272e16e85e3b38f9b
Jpablo55 <135130507+Jpablo55@users.noreply.github.com>
2025-06-17 12:58:44 -0500

    Add files via upload

---
+++ M3_S1/Logica/ClasificadorDeTriángulos.js
@@ -0,0 +1,12 @@
+let lado1 = parseFloat(prompt('Ingrese el primer lado del triángulo'));
+let lado2 = parseFloat(prompt('Ingrese el segundo lado del triángulo'));
+let lado3 = parseFloat(prompt('Ingrese el tercer lado del triángulo'));
+
+if(lado1 >= 0 && lado2 >= 0 && lado3 >= 0){
+    if(((lado1 + lado2) > lado3) && ((lado2 + lado3) > lado1) && ((lado1 + lado3) > lado2)){
+
+    }
+}
+else{
+    
+}
